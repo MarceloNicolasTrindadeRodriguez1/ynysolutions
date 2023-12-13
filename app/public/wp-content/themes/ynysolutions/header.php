43c0b0aaca2bac5414138536f5b4c9fb38dda853
MarceloNicolasTrindadeRodriguez <82178471+MarceloNicolasTrindadeRodriguez@users.noreply.github.com>
2023-12-13 08:39:24 -0500

    advancing

--- app/public/wp-content/themes/ynysolutions/header.php
+++ app/public/wp-content/themes/ynysolutions/header.php
@@ -10,11 +10,11 @@
 <head>
     <meta charset="<?php bloginfo('charset') ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head(); ?>
-    
+    <?php wp_head(); ?>    
     <title>YNY Solutions</title>
-
 </head>
 <body>
+<?php get_template_part('template-parts/header/navbar.php'); ?>
+
 
 
