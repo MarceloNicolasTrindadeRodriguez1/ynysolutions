1745efaa7f67eac007ec2123cea35bfd0dd7e815
MarceloNicolasTrindadeRodriguez <82178471+MarceloNicolasTrindadeRodriguez@users.noreply.github.com>
2023-12-13 08:36:00 -0500

    advancing

--- app/public/wp-content/themes/ynysolutions/header.php
+++ app/public/wp-content/themes/ynysolutions/header.php
@@ -16,12 +16,5 @@
 
 </head>
 <body>
-    <header>
-        <nav>
-            <li><a href="">Home</a></li>
-            <li><a href="">About Us</a></li>
-            <li><a href="">Contact Us</a></li>
-            <li><a href="">Store</a></li>
-        </nav>
-    </header>
+
 
---
+++ app/public/wp-content/themes/ynysolutions/template-parts/header/nav.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * Navbar
+ * @package ynysolutions
+ * 
+ */
+
+?>
+<header>
+        <nav>
+            <li><a href="">Home</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Contact Us</a></li>
+            <li><a href="">Store</a></li>
+        </nav>
+    </header>
